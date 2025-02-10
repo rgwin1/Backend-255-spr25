5adec1b83c23c5f04ffc4837a5da81f61ccd7b73
Ryan <rgwin1@ivytech.edu>
2025-02-09 22:10:11 -0500

    created first api route to get songs

--- app.js
+++ app.js
@@ -3,24 +3,36 @@
 //A: 
 //set.. this is similar to when we use our default tags in html
 const express = require('express'); //name of the express module can be used to do express things
+//cors is required in order to host a front end and backend on the same device
+var cors = require('cors')
 //activate or tell this app variable to be an express server
 const app = express(); //an activated express server. can you the express app functionality
+app.use(cors())
+const router = express.Router();
 
-//start the web server...app.listen(portnumber, function);
-app.listen(3000, function(){
-    console.log('listening on port 3000') //a log to make sure the server is running
-})
+router.get('/songs', function(req, res){
+    const songs = [{
+        title: "We Found Love",
+        artist: 'Rihanna',
+        popularity: 10,
+        genre: ['funk', 'boogie']
+    },
+    {
+        title: "Happy",
+        artist: "Pharrell Williams",
+        popularity: 10,
+        genre: ['soul', 'new soul']
+    }]
 
-
-//B: making an api using routes
-//GET or a regular request when someone goes to https://localhost:3000/hello...endpoint of url
-app.get("/hello", function(req, res){
-    //B1
-    res.send("<h1>Hello, Express</h1>")
+    //send this object to the frond end converting it to JSON
+    res.json(songs) //turns song into a json object and sends it
 });
 
-app.get("/goodbye", function(req, res){
-    //B1
-    res.send("<h1>Goodbye, Express</h1>")
-});
+//all requests that usually use an api start with /api... so the url would be localhost:3000/api/songs
+app.use('/api', router);
+app.listen(3000);
 
+//C
+
+
+//left off at time: 
